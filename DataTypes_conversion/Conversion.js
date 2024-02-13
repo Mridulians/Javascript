81b705e4a58ee5dd716eddd192d3c7b404381149
Mridulians <gmridul898@gmail.com>
2024-02-13 17:05:34 +0530

    datatype conversions

---
+++ DataTypes_conversion/Conversion.js
@@ -0,0 +1,34 @@
+// let score = "33aaa";
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let updation = Number(score)
+
+// console.log(typeof updation)
+// console.log(updation)
+
+
+// NOTE---->
+// "33" === 33
+// "33eee" = NaN
+
+
+// let vari = true
+
+// console.log(vari)
+// console.log(typeof vari)
+
+// let newVari = Number(vari)
+// console.log(newVari)
+// console.log(typeof newVari)
+
+
+// let some = 33
+
+// console.log(typeof some);
+
+// let someVari  = String(some)
+// console.log(typeof someVari)
+// console.log(`"${someVari}"`)
+
--- variables/Variables.js
+++ variables/Variables.js
@@ -1,1 +1,15 @@
 console.log("mridul")
+
+const accountId = 1212121;
+
+let accountName = "Mridul"
+
+var accountEmail = "gmridul898@gmail.com" 
+// prefer not to use "var" because of issue in block scope and functional scope.
+
+// accountId = 212121  const can't be modified
+
+accountName = "MSD"
+accountEmail = "google.com"
+
+console.table([accountId , accountName , accountEmail])
