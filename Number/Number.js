366925760748952ce84a5ac96ee6e16223a45470
Mridulians <gmridul898@gmail.com>
2024-02-14 19:47:58 +0530

    Nunber and date

---
+++ Number/Number.js
@@ -0,0 +1,31 @@
+const num = 400;
+
+const num1 = new Number(400)
+
+// console.log(num1)
+// console.log(num)
+
+// console.log(num1.toString())
+// console.log(typeof num1.toString())
+
+
+// console.log(Math.random())
+// console.log(Math.random()*10 +1)
+// console.log(Math.floor(Math.random()*10) +1)
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Date in js
+
+const newDate = new Date();
+console.log(newDate)
+
+console.log(typeof newDate)
+
+console.log(newDate.toDateString())
+console.log(newDate.toLocaleString())
+console.log(newDate.getDate())
+console.log(newDate.getMonth()+1)
+console.log(newDate.getFullYear())
